ee0717fffcdfaddeaf3fd0ce5b98ffe3c92399fb
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2025-04-12 07:46:46 +0530

    Update InventoryPage.js

--- frontend/src/components/InventoryPage.js
+++ frontend/src/components/InventoryPage.js
@@ -12,7 +12,7 @@ const InventoryPage = () => {
   // Fetch data from backend (MongoDB) and filter by godown name
   useEffect(() => {
     axios
-      .get("http://localhost:5000/api/delevery1") // Updated endpoint
+      .get("http://3.7.70.168:5000/api/delevery1") // Updated endpoint
       .then((response) => {
         console.log("API Response: ", response.data); // Debugging: Check API response
         
